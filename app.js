a7e44743ab0d9b257d558999cfd13abebc13ccbc
AkashPrsd <67496460+AkashPrsd@users.noreply.github.com>
2020-10-15 14:33:08 +0530

    'abhiiphotography'

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ let storage= multer.diskStorage({
 })
 let upload = multer({ storage: storage }).single("Image"); //Field name and max count
 
-let port = process.env.PORT ||8080;
-let url = process.DB_CONNECT || 'mongodb+srv://demonew:demo123456@cluster0.yw73h.mongodb.net/newproject?retryWrites=true&w=majority';
+let port = process.env.PORT || 8080;
+let url = process.env.DB_CONNECT || 'mongodb+srv://demonew:demo123456@cluster0.yw73h.mongodb.net/newproject?retryWrites=true&w=majority';
 
 mongoose.connect(
-  process.env.url ,
+  process.env.DB_CONNECT ,
   { useNewUrlParser: true, useUnifiedTopology: true,   useFindAndModify: false, },
   ()=> console.log('connected to db'));
 
