d1c2badb6b4f56e48a5dcb766d3ae125d155b5d2
AkashPrsd <67496460+AkashPrsd@users.noreply.github.com>
2020-10-15 22:08:32 +0530

    'abhiiphotography'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 const multer = require("multer");
 const dotenv = require("dotenv");
 
@@ -18,6 +19,9 @@ let storage= multer.diskStorage({
 })
 let upload = multer({ storage: storage }).single("Image"); //Field name and max count
 
+let port = 8080;
+let url = process.DB_CONNECT || 'mongodb+srv://demonew:demo123456@cluster0.yw73h.mongodb.net/newproject?retryWrites=true&w=majority';
+
 mongoose.connect(
   process.env.DB_CONNECT ,
   { useNewUrlParser: true, useUnifiedTopology: true,   useFindAndModify: false, },
@@ -34,7 +38,12 @@ let app = express()
 app.use(cors());
 app.use(express.json()) 
 app.use("/myimages", express.static("attach"))
+app.use(express.static(path.join(__dirname, './dist/photogallery')));
+app.use('/', express.static(path.join(__dirname, './dist/photogallery')));
 
+app.get('*', (req, res) =>{
+     res.sendFile(path.join(__dirname, './dist/photogallery/index.html'));
+   });//short method
 
 /*app.get("/server/getuploadImg", function(req, res) {
   imgModel.find({}, function(err, data) {
@@ -281,8 +290,8 @@ app.post("/server/uploadContactUs", async (req, res) =>{
 });
 
 /***** */
-app.listen(8080, () => {
-  console.log("app listeing on port:8080");
+app.listen(port, () => {
+  console.log("app listeing on port:", port);
 })
 // git add .
 //git commit -m 'abhiiphotography'
