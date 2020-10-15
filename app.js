d0ea96388f942888a4b966629a67cfdb1485cd90
AkashPrsd <67496460+AkashPrsd@users.noreply.github.com>
2020-10-15 12:56:21 +0530

    'abhiiphotography'

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ let app = express()
 app.use(cors());
 app.use(express.json()) 
 app.use("/myimages", express.static("attach"))
+app.use(express.static(path.join(__dirname, './dist/photogallery')))
 app.use('/', express.static(path.join(__dirname, './dist/photogallery')))
 
 
@@ -53,6 +54,10 @@ app.use('/', express.static(path.join(__dirname, './dist/photogallery')))
   const data = await imgModel.find();
      res.send({ data: data });
    });//short method*/
+
+   app.get("**", (req, res) =>{
+       res.sendFile(path.join(__dirname, 'dist/photogallery/index.html'));
+     });//short method
 
 app.get("/server/getuploadImg", async (req, res) =>{
   const data = await imgModel.find();
