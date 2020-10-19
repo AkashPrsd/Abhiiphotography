f4d579786c4431f1a8cb37566fb89b48f99215aa
AkashPrsd <67496460+AkashPrsd@users.noreply.github.com>
2020-10-19 23:16:03 +0530

    'abhiiphotography'

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ app.get('**', (req, res) =>{
 
 
     port = process.env.PORT || "3000";
-    server = process.env.MONGODB_URI || 'mongodb+srv://demonew:demo123456@cluster0.yw73h.mongodb.net/newproject?retryWrites=true&w=majority';
   
   
    
