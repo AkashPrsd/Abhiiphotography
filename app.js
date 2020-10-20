c48d8d146e9673acb5b746b1b91e3ee228332368
AkashPrsd <67496460+AkashPrsd@users.noreply.github.com>
2020-10-20 18:16:23 +0530

    'abhiiphotography'

--- app.js
+++ app.js
@@ -296,7 +296,7 @@ app.post("/server/uploadContactUs", async (req, res) =>{
 
 const port = process.env.PORT || 8080;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log("app listeing on port:", port);
 })  
 
